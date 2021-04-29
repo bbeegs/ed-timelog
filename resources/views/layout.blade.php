2d02461a9259daff4471110b6728da92cd7731c1
bbeegs <brianbeegan@protonmail.com>
2021-04-29 09:49:36 -0400

    responsive fixes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Patient Observation Tracker</title>
-	<link href="{{asset('css/app.css') }}" rel="stylesheet">
+	<link href="{{secure_asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
 	<nav class="navbar" role="navigation" aria-label="main navigation">
