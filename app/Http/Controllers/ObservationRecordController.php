ba1db3329432d94555ad810078466891cd9bfba2
bbeegs <brianbeegan@protonmail.com>
2021-05-17 07:45:44 -0400

    fix redirects

--- app/Http/Controllers/ObservationRecordController.php
+++ app/Http/Controllers/ObservationRecordController.php
@@ -96,7 +96,7 @@ class ObservationRecordController extends Controller
             'total_hours' => $diffTime
         ]);
         $record->save();
-        return redirect()->back(); 
+        return redirect(session('links')[2]); 
     }
 
     public function FormatDatesAndTimes($date, $time){
@@ -154,7 +154,7 @@ class ObservationRecordController extends Controller
             'observation_date' => $validated['observation_date'],
             'total_hours' => $bbb->format("%H:%I:%S")
         ]);
-        return redirect()->back(); 
+        return redirect(session('links')[2]); 
     }
 
     /**
